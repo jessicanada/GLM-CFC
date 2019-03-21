31a4a24a2c29758480880d1d699a0d15dfa33855
jessicanada <jnadalin@bu.edu>
2019-03-21 14:22:06 -0400

    added new sim

---
+++ new/MIvsRNewSignalUpdate.m
@@ -0,0 +1,53 @@
+addpath('Chaotic Systems Toolbox')
+
+% Example to show where GLM outperforms MI. In this example, we'll generate the 
+fixed_val = -1;
+%CFC_strength   = (0:.1:1);        % This controls "strength" of PAC.
+n_realizations = 1000;            % The number of times to re-run each CFC setting.
+
+PAC            = zeros(1, n_realizations);
+PAC_new        = zeros(1, n_realizations);
+CFC            = zeros(1, n_realizations);
+mi             = zeros(1, n_realizations);
+p_PAC          = zeros(1, n_realizations);
+p_CFC          = zeros(1, n_realizations);
+p_mi           = zeros(1, n_realizations);
+p_PAC_new      = zeros(1, n_realizations);
+
+%for i_CFC=1:length(CFC_strength)
+
+    for i_run=1:n_realizations
+        i_run
+        [XX,P,Vlo,Vhi,t] = simfun_mi_vs_r(1.5,fixed_val);
+        CFC(i_run) = XX.rcfc;                 % Compute GLM and save it.
+        PAC(i_run) = XX.rpac;
+        PAC_new(i_run) = XX.rpac_new;
+        p_PAC(i_run) = P.rpac;
+        p_CFC(i_run) = P.rcfc;
+        p_PAC_new(i_run) = P.rpac_new;
+        [MI,p_MI]  = modulation_index(Vlo, Vhi,'pvals');               % Compute MI and save it.
+        mi(i_run) = MI;
+        p_mi(i_run) = p_MI;
+        
+    end
+
+
+
+    ind1 = find(p_mi<.05);
+    sig_MI = length(ind1);
+    ind2 = find(p_CFC<.05);
+    sig_CFC = length(ind2);
+    ind3 = find(p_PAC<.05);
+    sig_PAC = length(ind3);
+    ind4 = find(p_PAC_new<.05);
+    sig_PAC_new = length(ind4);
+    
+strname = ['R_MI_Comparison_New_R_Definition_Update'];
+save(strname)
+
+% %%
+% CFC_strength   = (0:.1:1);
+% plot(CFC_strength,sig_MI,CFC_strength,sig_CFC,CFC_strength,sig_PAC_new,CFC_strength,sig_PAC,'LineWidth',2); legend('MI','CFC','PAC new','PAC old')
+% set(gca,'FontSize',14)
+% xlabel('Intensity'); ylabel('Significant Detections')
+
