91a47300c96249d79b2ff376c2668a780c9c11ef
jessicanada <jnadalin@bu.edu>
2019-03-29 16:38:31 -0400

    debug

--- new/what_makes_mi_increase.m
+++ new/what_makes_mi_increase.m
@@ -174,15 +174,12 @@
 
 addpath('Chaotic Systems Toolbox')
 
-N = 100;
-RPAC1 = zeros(1,N); p_RPAC1 = zeros(1,N);
-MI1 = zeros(1,N); p_MI1 = zeros(1,N);
-RPAC2 = zeros(1,N); p_RPAC2 = zeros(1,N);
-MI2 = zeros(1,N); p_MI2 = zeros(1,N);
-for i = 1:N
-    i
-% to simulate: positive coupling at pi/2, negative at -pi/2? or positive at
-% pi negative at 0, whatever
+M = 3;
+RPAC1 = zeros(1,M); p_RPAC1 = zeros(1,M);
+MI1 = zeros(1,M); p_MI1 = zeros(1,M);
+RPAC2 = zeros(1,M); p_RPAC2 = zeros(1,M);
+MI2 = zeros(1,M); p_MI2 = zeros(1,M);
+
 dt = 0.002;  Fs = 1/dt;  fNQ = Fs/2;        % Simulated time series parameters.
 N  = 20/dt+4000;
 
@@ -205,8 +202,13 @@ trans          = 0.15;                      % fractional width of transition zon
 f=[MINFREQ (1-trans)*locutoff/fNQ locutoff/fNQ hicutoff/fNQ (1+trans)*hicutoff/fNQ 1];
 m=[0       0                      1            1            0                      0];
 filtwts_hi = firls(filtorder,f,m);             % get FIR filter coefficients
-                          % # steps to simulate, making the duration 20s
 
+                       
+for i = 1:M
+    i
+    
+    N  = 20/dt+4000;
+    
 % Make the data.
 Vpink = make_pink_noise(1,N,dt);            % First, make pink noise.
 Vpink = Vpink - mean(Vpink);                % ... with zero mean.
@@ -221,7 +223,6 @@ Vhi = filtfilt(filtwts_hi,1,Vpink);            % Define high freq band activity.
 Vlo = Vlo(2001:end-2000);
 Vhi = Vhi(2001:end-2000);
 t   = (1:length(Vlo))*dt;
-N   = length(Vlo);
 
 % Find peaks of the low freq activity.
 
@@ -246,7 +247,8 @@ Vhi = Vhi.*(1+pac_mod.*s);
 Vhi1 = Vhi.*(1+aac_mod.*AmpLo/max(AmpLo)); %with AAC
 Vhi2 = Vhi; %without AAC
 
-Vpink2 = make_pink_noise(1,N,dt);                   
+Vpink2 = make_pink_noise(1,N,dt);
+Vpink2 = Vpink2(2001:end-2000);
 noise_level = 0.01;
 
 %Vlo = Vlo.*[1*ones(length(Vhi)/2,1);1*ones(length(Vhi)/2,1)]'; %increase low frequency amplitude in post
@@ -266,15 +268,15 @@ Vhi2 = filtfilt(filtwts_hi,1,V2);            % Define high freq band activity.
 
 %with AAC
 [XX1,P1] = glmfun(Vlo1, Vhi1, 'empirical','none',.05);
-[MI1,MP1] = modulation_index(Vlo1,Vhi1,'pvals');
+[mi1,mp1] = modulation_index(Vlo1,Vhi1,'pvals');
 RPAC1(i) = XX1.rpac_new; p_RPAC1(i) = P1.rpac_new;
-MI1(i) = MI1; p_MI1(i) = MP1;
+MI1(i) = mi1; p_MI1(i) = mp1;
 
 %without AAC
 [XX2,P2] = glmfun(Vlo2, Vhi2, 'empirical','none',.05);
-[MI2,MP2] = modulation_index(Vlo2,Vhi2,'pvals');
+[mi2,mp2] = modulation_index(Vlo2,Vhi2,'pvals');
 RPAC2(i) = XX2.rpac_new; p_RPAC2(i) = P2.rpac_new;
-MI2(i) = MI2; p_MI2(i) = MP2;
+MI2(i) = mi2; p_MI2(i) = mp2;
 end
 
 save('R_MI_Comparison_Increase_AAC','RPAC1','RPAC2','p_RPAC1','p_RPAC2','MI1','MI2','p_MI1','p_MI2')
