397354000300d73591f2c1183631afb8e29bea8b
jessicanada <jnadalin@bu.edu>
2019-04-01 09:57:40 -0400

    increased number of iterations

--- new/what_makes_mi_increase.m
+++ new/what_makes_mi_increase.m
@@ -174,7 +174,7 @@
 
 addpath('Chaotic Systems Toolbox')
 
-M = 3;
+M = 100;
 RPAC1 = zeros(1,M); p_RPAC1 = zeros(1,M);
 MI1 = zeros(1,M); p_MI1 = zeros(1,M);
 RPAC2 = zeros(1,M); p_RPAC2 = zeros(1,M);
