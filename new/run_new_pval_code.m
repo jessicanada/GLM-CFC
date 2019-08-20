b240a389922bfe5d3aad22b5789ed07f9592772f
jessicanada <jnadalin@bu.edu>
2019-08-20 17:23:58 -0400

    updated to include other pvals

--- new/run_new_pval_code.m
+++ new/run_new_pval_code.m
@@ -1,9 +1,11 @@
 K = 100;
-pval_nochange = zeros(1,K);
+pval_nochange_w = zeros(1,K);
+pval_nochange_wo = zeros(1,K);
 for i = 1:K
-    [Vlo_pre,Vhi_pre,Vlo_post,Vhi_post] = simulate_prepost(0,0,0,0);
+    [Vlo_pre,Vhi_pre,Vlo_post,Vhi_post] = simulate_prepost(3,3,0,0);
     [~,~,I] = glmfun_with_indicator_update(Vlo_pre,Vlo_post,Vhi_pre,Vhi_post,'none','none',.05);
-    pval_nochange(i) = I.d_pval;
+    pval_nochange_w(i) = I.d_w_pval;
+    pval_nochange_wo(i) = I.d_wo_pval;
     i
 end
 
